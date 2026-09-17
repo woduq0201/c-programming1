8445371c664c1a7a225d5366f53b451c69a9416e
woduq0201 <goalpe123@naver.com>
2026-09-17 13:38:57 +0900

    Add files via upload

---
+++ w2 실습1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int val = 21;
+	int val_minus = -21;
+	int result = 0;
+
+	result = val + val_minus;
+	printf("result is %d\n", result);
+	return 0;
+}
---
+++ w2 실습2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main2(void)
+{
+	float num = 85.125;
+	double dnum = 85.125;
+
+	return 0; 
+}
